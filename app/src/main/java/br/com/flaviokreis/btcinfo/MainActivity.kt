e5aec6764252dde072e117aa7e35b5bb69496652
Flavio Kreis <flaviokreis@gmail.com>
2019-09-22 09:59:36 -0300

    Add retrofit, models and services

--- app/src/main/java/br/com/flaviokreis/btcinfo/MainActivity.kt
+++ app/src/main/java/br/com/flaviokreis/btcinfo/MainActivity.kt
@@ -1,12 +1,23 @@
 package br.com.flaviokreis.btcinfo
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import br.com.flaviokreis.btcinfo.services.BitcoinServiceApi
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        BitcoinServiceApi.getChart { chart, error ->
+            println("Chart: $chart")
+            println("Chart error: $error")
+        }
+
+        BitcoinServiceApi.getStats { stats, error ->
+            println("Stats: $stats")
+            println("Stats error: $error")
+        }
     }
 }
---
+++ app/src/main/java/br/com/flaviokreis/btcinfo/models/BitcoinChartValue.kt
@@ -0,0 +1,6 @@
+package br.com.flaviokreis.btcinfo.models
+
+data class BitcoinChartValue(
+    val x: Double = 0.0,
+    val y: Double = 0.0
+)
